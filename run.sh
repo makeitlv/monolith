36509c8b1d1d7909c32142763e36189d2cec0439
Viktor Vipolzov <viktor.vipolzov@gmail.com>
2023-03-10 21:59:56 +0200

    Installed code analyse + code quality tools

--- run.sh
+++ run.sh
@@ -9,6 +9,19 @@ if [ $# -gt 0 ]; then
         shift 1
         docker-compose exec php composer "$@"
         docker-compose exec php chown -R $(id -u):$(id -g) .
+    elif [ "$1" == "qa" ]; then
+        shift 1
+        docker-compose exec php php -d memory_limit=-1 ./bin/phpcs.phar --standard=phpcs.xml
+        docker-compose exec php php -d memory_limit=-1 ./bin/phpcpd.phar --fuzzy src/ config/
+        docker-compose exec php php -d memory_limit=-1 ./bin/psalm.phar
+        docker-compose exec php php -d memory_limit=-1 ./bin/deptrac.phar analyse
+    elif [ "$1" == "test" ]; then
+        shift 1
+        docker-compose exec php php -d memory_limit=-1 bin/console cache:clear --env=test
+        docker-compose exec php php -d memory_limit=-1 bin/console doctrine:database:create --env=test --if-not-exists
+        docker-compose exec php php -d memory_limit=-1 bin/console doctrine:schema:drop --env=test --force
+        docker-compose exec php php -d memory_limit=-1 bin/console doctrine:schema:create --env=test
+        docker-compose exec php php -d memory_limit=-1 ./bin/phpunit "$@"
     else
         echo "Unknown command"
     fi
