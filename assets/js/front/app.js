8ef212d5465e606ca67c80eb07e1157882d04f50
Viktor Vipolzov <viktor.vipolzov@gmail.com>
2023-04-06 20:05:40 +0300

    Changed js/css folder structure

--- assets/js/front/app.js
+++ assets/js/front/app.js
@@ -6,7 +6,7 @@
  */
 
 // any CSS you import will output into a single css file (app.css in this case)
-import "./styles/front.css";
+import "./../../css/front/main.css";
 
 // start the Stimulus application
 import "./bootstrap";
