af38fad87136d48b49700caa021dc2813605c562
Viktor Vipolzov <viktor.vipolzov@gmail.com>
2023-04-08 22:09:42 +0300

    Added dashboard template + small admin panel configuration

--- src/Dashboard/Presentation/Controller/Back/DashboardController.php
+++ src/Dashboard/Presentation/Controller/Back/DashboardController.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Dashboard\Presentation\Controller\Back;
 
+use App\Common\Presentation\Translation\Back\TranslatableMessage;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
@@ -15,33 +16,22 @@ class DashboardController extends AbstractDashboardController
     #[Route("/admin/dashboard", name: "back.dashboard")]
     public function index(): Response
     {
-        return parent::index();
-
-        // Option 1. You can make your dashboard redirect to some common page of your backend
-        //
-        // $adminUrlGenerator = $this->container->get(AdminUrlGenerator::class);
-        // return $this->redirect($adminUrlGenerator->setController(OneOfYourCrudController::class)->generateUrl());
-
-        // Option 2. You can make your dashboard redirect to different pages depending on the user
-        //
-        // if ('jane' === $this->getUser()->getUsername()) {
-        //     return $this->redirect('...');
-        // }
-
-        // Option 3. You can render some custom template to display a proper dashboard with widgets, etc.
-        // (tip: it's easier if your template extends from @EasyAdmin/page/content.html.twig)
-        //
-        // return $this->render('some/path/my-dashboard.html.twig');
+        return $this->render("back/page/dashboard/index.html.twig");
     }
 
     public function configureDashboard(): Dashboard
     {
-        return Dashboard::new()->setTitle("App");
+        return Dashboard::new()
+            ->setTitle("AdminPanel")
+            ->setTranslationDomain(TranslatableMessage::DOMAIN);
     }
 
     public function configureMenuItems(): iterable
     {
+        yield MenuItem::section("Main");
         yield MenuItem::linkToDashboard("Dashboard", "fa fa-home");
-        // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
+
+        yield MenuItem::section("Other");
+        yield MenuItem::linkToUrl("Website", "fa fa-globe", "/");
     }
 }
