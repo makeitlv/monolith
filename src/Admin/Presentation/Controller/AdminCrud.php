8d3abec11f0b8e6d460db363a0a76f6d50f07297
Viktor Vipolzov <viktor.vipolzov@gmail.com>
2023-05-01 12:38:38 +0300

    Small translation fix

--- src/Admin/Presentation/Controller/AdminCrud.php
+++ src/Admin/Presentation/Controller/AdminCrud.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Admin\Presentation\Controller;
 
 use App\Admin\Presentation\Model\AdminModel;
+use App\Common\Presentation\Translation\Back\TranslatableMessage;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 
 class AdminCrud
@@ -13,10 +14,10 @@ class AdminCrud
     {
         return $crud
             ->setPageTitle(Crud::PAGE_DETAIL, static function (AdminModel $admin) {
-                return sprintf("Display %s", $admin->email);
+                return new TranslatableMessage("Display %email%", ["%email%" => $admin->email]);
             })
             ->setPageTitle(Crud::PAGE_EDIT, static function (AdminModel $admin) {
-                return sprintf("Edit %s", $admin->email);
+                return new TranslatableMessage("Edit %email%", ["%email%" => $admin->email]);
             })
             ->setSearchFields(["email", "firstname", "lastname"])
             ->setEntityLabelInSingular("Admin")
