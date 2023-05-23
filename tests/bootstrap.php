eb563f409b62fbfb8e8ba9b87fbcc7ce351fa558
Viktor Vipolzov <viktor.vipolzov@gmail.com>
2023-05-23 20:45:43 +0300

    Small formatting

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,34 +2,20 @@
 
 use Symfony\Component\Dotenv\Dotenv;
 
-require dirname(__DIR__) . '/vendor/autoload.php';
+require dirname(__DIR__) . "/vendor/autoload.php";
 
-if (file_exists(dirname(__DIR__) . '/config/bootstrap.php')) {
-    require dirname(__DIR__) . '/config/bootstrap.php';
-} elseif (method_exists(Dotenv::class, 'bootEnv')) {
-    (new Dotenv())->bootEnv(dirname(__DIR__) . '/.env');
+if (file_exists(dirname(__DIR__) . "/config/bootstrap.php")) {
+    require dirname(__DIR__) . "/config/bootstrap.php";
+} elseif (method_exists(Dotenv::class, "bootEnv")) {
+    (new Dotenv())->bootEnv(dirname(__DIR__) . "/.env");
 }
 
-passthru(sprintf(
-    'php "%s/../bin/console" cache:clear --env=%s',
-    __DIR__,
-    $_ENV['APP_ENV'],
-));
+passthru(sprintf('php "%s/../bin/console" cache:clear --env=%s', __DIR__, $_ENV["APP_ENV"]));
 
-passthru(sprintf(
-    'php "%s/../bin/console" doctrine:database:create --env=%s --if-not-exists',
-    __DIR__,
-    $_ENV['APP_ENV'],
-));
+passthru(
+    sprintf('php "%s/../bin/console" doctrine:database:create --env=%s --if-not-exists', __DIR__, $_ENV["APP_ENV"])
+);
 
-passthru(sprintf(
-    'php "%s/../bin/console" doctrine:schema:drop --env=%s --force',
-    __DIR__,
-    $_ENV['APP_ENV'],
-));
+passthru(sprintf('php "%s/../bin/console" doctrine:schema:drop --env=%s --force', __DIR__, $_ENV["APP_ENV"]));
 
-passthru(sprintf(
-    'php "%s/../bin/console" doctrine:schema:create --env=%s',
-    __DIR__,
-    $_ENV['APP_ENV'],
-));
+passthru(sprintf('php "%s/../bin/console" doctrine:schema:create --env=%s', __DIR__, $_ENV["APP_ENV"]));
