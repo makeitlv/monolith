08420c00882f23f4d3c49a386baec00bf16236c4
Viktor Vipolzov <viktor.vipolzov@gmail.com>
2023-05-27 12:31:07 +0300

    Updated tests

--- tests/Notification/Application/UseCase/Event/Admin/AdminCreated/SendWelcomeNotifierHandlerTest.php
+++ tests/Notification/Application/UseCase/Event/Admin/AdminCreated/SendWelcomeNotifierHandlerTest.php
@@ -7,8 +7,11 @@ namespace App\Tests\Notification\Application\UseCase\Event\Admin\AdminCreated;
 use App\Admin\Domain\Event\AdminCreatedEvent;
 use App\Common\Domain\Bus\Event\EventBus;
 use App\Tests\FunctionalTestCase;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\DBAL\Connection;
 use Zenstruck\Mailer\Test\InteractsWithMailer;
 use Zenstruck\Messenger\Test\InteractsWithMessenger;
+use Symfony\Component\Messenger\Transport\Serialization\SerializerInterface;
 
 final class SendWelcomeNotifierHandlerTest extends FunctionalTestCase
 {
@@ -23,26 +26,72 @@ final class SendWelcomeNotifierHandlerTest extends FunctionalTestCase
             new AdminCreatedEvent(
                 ($uuid = "de28e1d2-9a57-48b0-af1b-ae54b39e0801"),
                 ($email = "admin@admin.com"),
-                ($firstname = "Admin"),
-                ($lastname = "Admin")
+                ($name = "Admin Admin"),
+                ($password = "Password")
             )
         );
 
-        $this->transport("async")
-            ->queue()
-            ->assertNotEmpty();
-        $this->transport("async")
-            ->queue()
-            ->assertCount(1);
-        $this->transport("async")
-            ->queue()
+        $asyncTrasport = $this->transport("async");
+        $mailer = $this->mailer();
+
+        $asyncTrasport->queue()->assertNotEmpty();
+        $asyncTrasport->queue()->assertCount(1);
+        $asyncTrasport->queue()->assertContains(AdminCreatedEvent::class);
+
+        $mailer->assertNoEmailSent();
+
+        $asyncTrasport->process();
+
+        $mailer->assertSentEmailCount(1);
+        $mailer->assertEmailSentTo($email, "Welcome to the team!");
+    }
+
+    public function testEmailNotSent(): void
+    {
+        /** @var EventBus $eventBus */
+        $eventBus = $this->getContainer()->get(EventBus::class);
+        $eventBus->publish(
+            new AdminCreatedEvent(
+                ($uuid = "de28e1d2-9a57-48b0-af1b-ae54b39e0801"),
+                ($email = "adminadmin.com"),
+                ($name = "Admin Admin"),
+                ($password = "Password")
+            )
+        );
+
+        $asyncTrasport = $this->transport("async");
+
+        $asyncTrasport->queue()->assertNotEmpty();
+        $asyncTrasport->queue()->assertCount(1);
+        $asyncTrasport->queue()->assertContains(AdminCreatedEvent::class);
+
+        $asyncTrasport->process();
+        $asyncTrasport
+            ->rejected()
+            ->assertCount(4)
             ->assertContains(AdminCreatedEvent::class);
 
-        $this->mailer()->assertNoEmailSent();
+        $asyncTrasport->queue()->assertEmpty();
 
-        $this->transport("async")->process();
+        /** @var Connection $connection */
+        $connection = $this->getContainer()
+            ->get(EntityManagerInterface::class)
+            ->getConnection();
 
-        $this->mailer()->assertSentEmailCount(1);
-        $this->mailer()->assertEmailSentTo($email, "Welcome to the team!");
+        $messageData = $connection
+            ->prepare("SELECT * FROM messenger_messages LIMIT 1")
+            ->executeQuery()
+            ->fetchAssociative();
+
+        /** @var SerializerInterface $serializer */
+        $serializer = $this->getContainer()->get(SerializerInterface::class);
+        /** @var AdminCreatedEvent $message */
+        $message = $serializer->decode($messageData)->getMessage();
+
+        self::assertEquals($uuid, $message->uuid);
+        self::assertEquals($email, $message->email);
+        self::assertEquals($name, $message->name);
+        self::assertEquals($password, $message->plainPassword);
+        self::assertNull($message->confirmationToken);
     }
 }
